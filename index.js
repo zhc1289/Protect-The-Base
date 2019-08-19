6797b7afdcd32ca705bf449ba11bba9178ad321b
zhi <zhc1289@gmail.com>
2019-08-19 12:56:56 -0400

    added movement for hero

---
+++ index.js
@@ -0,0 +1,28 @@
+function move(e) {
+    var key = e.keyCode,
+    heroId = document.getElementById('hero'),
+    hero = {
+        vertical: function() {
+            var y = parseInt(getComputedStyle(heroId).top);
+            if (key == 38) {
+                y -= 4;
+            } else if (key == 40) {
+                y += 4;
+            }
+            return y
+        },
+        horizontal: function() {
+            var x = parseInt(getComputedStyle(heroId).left);
+            if (key == 37) {
+                x -= 4;
+            } else if (key == 39) {
+                x += 4;
+            }
+            return x
+        }
+    };
+    heroId.style.top = (hero.vertical()) + "px";
+    heroId.style.left = (hero.horizontal()) + "px";
+}
+
+document.addEventListener('keydown', move);
