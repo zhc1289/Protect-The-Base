8e5c5b2595c4b2b2230aed5097d513bbc98e0f8f
zhi <zhc1289@gmail.com>
2019-09-02 15:09:29 -0400

    cleanup

--- canvas.js
+++ canvas.js
@@ -3,14 +3,9 @@ var wall = canvas.getContext('2d');
 
 wall.fillStyle = 'rgb(147, 58, 22)';
 
-for (let j = 0; j < 121; j += 24) {
+for (let j = 0; j < 120; j += 24) {
     for (let i = 0; i < 126; i += 5) {
         wall.fillRect(i, j, 4, 10);
-    }
-}
-
-for (let j = 12; j < 121; j += 24) {
-    for (let i = 0; i < 126; i += 5) {
-        wall.fillRect(i + 2, j, 4, 10);
+        wall.fillRect(i + 2, j + 12, 4, 10);
     }
 }
