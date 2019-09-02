641189d41e36fd73d297593ca024cc3c616799ef
zhi <zhc1289@gmail.com>
2019-09-02 15:05:25 -0400

    brick wall

--- canvas.js
+++ canvas.js
@@ -1,5 +1,16 @@
 var canvas = document.getElementById('wall');
-var wall = canvas.getContext('2d')
+var wall = canvas.getContext('2d');
 
-wall.fillStyle = 'rgb(200,0,0)';
-wall.fillRect(0, 0, 150, 150);
+wall.fillStyle = 'rgb(147, 58, 22)';
+
+for (let j = 0; j < 121; j += 24) {
+    for (let i = 0; i < 126; i += 5) {
+        wall.fillRect(i, j, 4, 10);
+    }
+}
+
+for (let j = 12; j < 121; j += 24) {
+    for (let i = 0; i < 126; i += 5) {
+        wall.fillRect(i + 2, j, 4, 10);
+    }
+}
