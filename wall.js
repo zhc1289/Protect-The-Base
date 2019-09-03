18c67a2935fc55824574cd413d849925f09de6dc
zhi <zhc1289@gmail.com>
2019-09-03 15:07:07 -0400

    vibrant gold counter

--- wall.js
+++ wall.js
@@ -9,3 +9,11 @@ for (let j = 0; j < 61; j += 10) {
         wall.fillRect(i + 2, j + 5, 10, 4);
     }
 }
+
+var gold = document.getElementById('drawGold');
+var goldBars = gold.getContext('2d');
+
+goldBars.fillStyle = 'rgb(255, 215, 0)';
+goldBars.fillRect(7, 0, 12, 4);
+goldBars.fillRect(0, 5, 12, 4);
+goldBars.fillRect(13, 5, 12, 4)
