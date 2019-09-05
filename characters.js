f3b9d73a323ece46568bb4191ee86af60890f852
zhi <zhc1289@gmail.com>
2019-09-05 13:21:25 -0400

    creep head art

--- characters.js
+++ characters.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById('drawHero');
 var heroArt = canvas.getContext('2d');
 
 heroArt.fillStyle = 'rgb(0, 250, 0)';
-heroArt.fillRect(0, 20, 35, 50);
+heroArt.fillRect(0, 0, 35, 25);
 
 function drawLine(x1, y1, x2, y2){
     heroArt.beginPath();
@@ -14,7 +14,7 @@ function drawLine(x1, y1, x2, y2){
 // hero head
 heroArt.beginPath();
 // (centerX, centerY, radius, startAngle in radians, endAngle in radians, counterclockwise or clockwise boolean)
-heroArt.arc(canvas.width / 2, canvas.height / 2, 3, 0, 2 * Math.PI, false);
+heroArt.arc(canvas.width / 2, canvas.height / 5, 3, 0, 2 * Math.PI, false);
 heroArt.fillStyle = 'red';
 heroArt.fill();
 heroArt.lineWidth = 3;
@@ -23,7 +23,7 @@ heroArt.stroke();
 
 // hero body
 heroArt.beginPath();
-heroArt.arc(canvas.width / 2, canvas.height / 2 + 9, 4, 0, 2 * Math.PI, false);
+heroArt.arc(canvas.width / 2, canvas.height / 2 + 1, 4, 0, 2 * Math.PI, false);
 heroArt.fillStyle = 'red';
 heroArt.fill();
 heroArt.lineWidth = 3.5;
@@ -32,9 +32,49 @@ heroArt.stroke();
 
 // hero legs?
 heroArt.beginPath();
-heroArt.arc(canvas.width / 2, canvas.height / 2 + 18.5, 3, 0, 2 * Math.PI, false);
+heroArt.arc(canvas.width / 2, canvas.height / 2 + 9, 3, 0, 2 * Math.PI, false);
 heroArt.fillStyle = 'blue';
 heroArt.fill();
 heroArt.lineWidth = 4;
 heroArt.strokeStyle = '#003300';
 heroArt.stroke();
+
+var canvas = document.getElementById('drawCreep');
+var creepArt = canvas.getContext('2d');
+
+creepArt.fillStyle = 'rgb(211, 211, 211)';
+creepArt.fillRect(0, 0, 24, 36);
+
+// creep head polygon
+creepArt.fillStyle = 'rgb(191, 0, 0)';
+creepArt.beginPath();
+creepArt.moveTo(9, 8);
+creepArt.lineTo(15, 8);
+creepArt.lineTo(18, 18);
+creepArt.lineTo(15, 28);
+creepArt.lineTo(9, 28);
+creepArt.lineTo(6, 18);
+creepArt.closePath();
+creepArt.fill();
+
+//creep horns
+creepArt.beginPath();
+creepArt.moveTo(8, 14);
+creepArt.lineTo(8, 3);
+creepArt.strokeStyle = 'rgb(191, 0, 0)';
+creepArt.stroke();
+
+creepArt.beginPath();
+creepArt.moveTo(8, 3);
+creepArt.lineTo(10, 1);
+creepArt.stroke();
+
+creepArt.beginPath();
+creepArt.moveTo(16, 14);
+creepArt.lineTo(16, 3);
+creepArt.stroke();
+
+creepArt.beginPath();
+creepArt.moveTo(16, 3);
+creepArt.lineTo(14, 1);
+creepArt.stroke();
